8d9e0e4191dcc3c0797fde53fc99ce77e7e89681
J4EH00N <wogns7930@naver.com>
2025-09-24 10:22:29 +0900

    v0.1.1

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import React from 'react'
 export default function HomePage() {
   return (
     <div>
-      <h1>welcome Home!</h1>
-      <p>hello</p>
+      <h1 className="text-4xl font-bold">welcome Home!</h1>
+      <p>clerk is good!</p>
     </div>
   )
 }
-
